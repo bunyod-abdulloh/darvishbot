f5d6e43022add1723b8ef071f1552e727a0ddea2
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2025-01-05 10:33:20 +0500

    Commited

--- handlers/private/start.py
+++ handlers/private/start.py
@@ -2,6 +2,7 @@ import asyncpg.exceptions
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.builtin import CommandStart
+from magic_filter import F
 
 from keyboards.default.user_buttons import main_dkb
 from loader import dp, db
@@ -20,3 +21,11 @@ async def bot_start(message: types.Message, state: FSMContext):
         await message.answer(text=f"{e}")
         pass
     await state.finish()
+
+
+@dp.message_handler(F.text == "🏡 Bosh sahifa")
+async def main_page_handle(message: types.Message, state: FSMContext):
+    await state.finish()
+    await message.answer(
+        text=message.text, reply_markup=main_dkb
+    )
